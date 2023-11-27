bc5bf101f93f0fee67cb6b6ba3d0a3277fb7a67a
zander37 <149011001+zander37@users.noreply.github.com>
2023-11-26 20:28:39 -0500

    Update sudoku_generator.py

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -189,6 +189,23 @@ class SudokuGenerator:
     def remove_cells(self):
         pass
 
+class Cell():
+
+    def __init__(self, value, row, col, screen):
+        self.value = value
+        self.row = row
+        self.col = col
+        self.screen = screen
+
+    def set_cell_value(self, value):
+        self.value = value
+
+    def set_sketched_value(self, value):
+        self.value = value
+
+    def draw(self):
+        pass
+
 '''
 DO NOT CHANGE
 Provided for students
